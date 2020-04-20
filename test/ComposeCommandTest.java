f37148c0cb87868b9f99776477dd166f808c0eab
ds15abt <dylanshepherd.james@gmail.com>
2020-04-20 02:30:01 +0200

    Last commit for the day/night,working and implement Command patt and also set up tests, START CODING MORE TESTS

---
+++ test/ComposeCommandTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author user1
+ */
+public class ComposeCommandTest {
+    
+    public ComposeCommandTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Test of execute method, of class ComposeCommand.
+     */
+    @Test
+    public void testExecute() {
+    }
+    
+}
